dcdcf0924a0031a892b25d95d3f34dbba419331b
Pasha Stetsenko <stpasha@gmail.com>
2023-10-24 20:55:21 -0700

    Add a test for Italic

--- tests/nodes/Italic.test.ts
+++ tests/nodes/Italic.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { Document, Paragraph, Italic, Text } from "../../src/nodes.js";
+import { Document, Paragraph, Italic, Text, Header } from "../../src/nodes.js";
 
 test("Simple italic", () => {
   const ast = new Document([
@@ -15,7 +15,39 @@ test("Simple italic", () => {
       "",
     ].join("\n")
   );
+  expect(ast.toWikimark()).toBe("\n*This text is italic*\n");
+});
+
+test("Italic and loose stars", () => {
+  const ast = new Document([
+    new Header(2, [new Italic([new Text("Italic header")])]),
+    new Paragraph([
+      new Text("Normal text (with *s) "),
+      new Italic([new Text("ita*lic")]),
+      new Text(" more text"),
+    ]),
+  ]);
+  expect(ast.toDebugTree()).toBe(
+    [
+      "Document:",
+      "  Header[2]:",
+      "    Italic:",
+      "      Text: Italic header",
+      "  Paragraph:",
+      "    Text: Normal text (with *s) ",
+      "    Italic:",
+      "      Text: ita*lic",
+      "    Text:  more text",
+      ""
+    ].join("\n")
+  );
   expect(ast.toWikimark()).toBe(
-    "\n*This text is italic*\n"
-  )
+    [
+      "",
+      "## *Italic header*",
+      "",
+      "Normal text (with \\*s) *ita\\*lic* more text",
+      ""
+    ].join("\n")
+  );
 });
